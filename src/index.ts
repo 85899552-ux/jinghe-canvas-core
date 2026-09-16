fd0ee45f03557d782bab6614a11d5bd07214cceb
85899552-ux <85899552@qq.com>
2026-09-16 14:29:53 +0800

    Add package namespace exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * as connections from './lib/canvasNodeConnections.ts';
+export * as imageReferences from './lib/imagePromptReferences.ts';
+export * as minimap from './lib/canvasMinimap.ts';
+export * as gestures from './lib/canvasPointerGesture.ts';
+export * as mentions from './lib/promptMentions.ts';
+export type * from './types/canvas.ts';
